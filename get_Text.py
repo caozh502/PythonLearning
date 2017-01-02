0613127ca7a9877e7ca50002db8cdaa9d1ca7fa6
caozh502 <328776293@qq.com>
2017-01-02 12:04:31 +0800

    delete len()

--- get_Text.py
+++ get_Text.py
@@ -4,4 +4,4 @@ from urllib import urlopen
 html=urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj=BeautifulSoup(html,"html.parser")
 namelist=bsObj.findAll(text="the prince")
-print(len(namelist))
+print(namelist)
